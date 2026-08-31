05f73e3843f550f43592b5fb76cd47c10c48a576
anjanaunnip <154441511+anjanaunnip@users.noreply.github.com>
2026-08-31 11:25:56 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,5 @@ echo "Starting Gunicorn..."
 # on CPU, and the default 30s worker timeout will kill the request mid-inference.
 exec gunicorn agrivision.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 3 \
+    --workers 1 \
     --timeout 120
